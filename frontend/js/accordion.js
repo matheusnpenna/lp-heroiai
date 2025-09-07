aaf93691c3dcef3aa8e613361a31ec207db47cb6
Matheus Penna <matheusnpenna@gmail.com>
2025-09-07 15:28:58 -0300

    fix collapse and update version

--- frontend/js/accordion.js
+++ frontend/js/accordion.js
@@ -1,30 +1,25 @@
 const FAQs = document.querySelectorAll('.accordion-item');
 
 if (FAQs && FAQs.length) {
-  FAQs.forEach((_, idx) => {
-    const i = idx + 1
-
-    document.getElementById(`accordion-heading-${i}`).addEventListener('click', function() {
-      const aBody = document.getElementById(`accordion-body-${i}`)
-      const aHeading = document.getElementById(`accordion-heading-${i}`)
+  FAQs.forEach((el, indexToShow) => {
+    el.addEventListener('click', function() {
+      const aBody = el.querySelector(`[id^="accordion-body"]`)
+      const aHeading = el.querySelector(`[id^="accordion-heading"]`)
   
       const isCollapsed = aBody.classList.contains('hidden') && aHeading.classList.contains('collapsed')
   
       if (isCollapsed) {
-        aBody.classList.remove('hidden')
-        aHeading.classList.remove('collapsed')
+        aBody.classList.remove('hidden');
+        aHeading.classList.remove('collapsed');
       } else {
         aBody.classList.add('hidden');
-        aHeading.classList.add('collapsed')
+        aHeading.classList.add('collapsed');
       }
   
-      FAQs.forEach((_, index) => { 
-        const indexToHidden = index + 1;
-        if (indexToHidden != i) {
-          document.getElementById(`accordion-body-${indexToHidden}`)
-          .classList.add('hidden')
-          document.getElementById(`accordion-heading-${indexToHidden}`)
-          .classList.add('collapsed')
+      FAQs.forEach((elToHidden, indexToHidden) => { 
+        if (indexToHidden != indexToShow) {
+          elToHidden.querySelector(`[id^="accordion-body"]`).classList.add('hidden')
+          elToHidden.querySelector(`[id^="accordion-heading"]`).classList.add('collapsed')
         }
       })
     })
