4833b16ab6ed3c9ac5ed97be9fa500a04e839e14
Matheus Penna <matheusnpenna@gmail.com>
2025-09-09 16:06:27 -0300

    finish page

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -26,26 +26,28 @@ if (FAQs && FAQs.length) {
   })
 }
 
-const glide = new Glide('.glide', {
-  type: 'carousel',
-  startAt: 0,
-  focusAt: 'center',
-  perView: 4,
-  autoplay: 5000,
-  breakpoints: {
-    1400: {
-      perView: 3
-    },
-    991: {
-      perView: 2
-    },
-    768: {
-      perView: 1
+if (window.innerWidth > 768) {
+  const glide = new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    focusAt: 'center',
+    perView: 4,
+    autoplay: 5000,
+    breakpoints: {
+      1400: {
+        perView: 3
+      },
+      991: {
+        perView: 2
+      },
+      768: {
+        perView: 1
+      }
     }
-  }
-})
-
-glide.mount()
+  })
+  
+  glide.mount()
+}
 const yearEl = document.querySelector('.footer__divider-info__year')
 
 if (yearEl) {
